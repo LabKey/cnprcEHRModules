6b2839690cd40523d55a068636bb2c0a448f00da
Binal Patel <binalp@labkey.com>
2016-05-16 18:04:04 +0000

    Spec 26213: Added logic to show death dates only for dead animals.

--- cnprc_ehr/resources/queries/cnprcSrc/demographics.sql
+++ cnprc_ehr/resources/queries/cnprcSrc/demographics.sql
@@ -4,9 +4,9 @@ AN_ACQ_DATE,
 AN_SEX As gender,
 AN_BIRTHPLACE_GEOG AS geographic_origin,
 AN_BIRTH_DATE AS birth,
-AN_LOCATION_DATE AS death,
+(CASE WHEN AN_LOCATION_PREFIX = '0000' THEN AN_LOCATION_DATE ELSE NULL END) AS death,
 AN_SP_CODE AS species,
-(CASE WHEN AN_LOCATION_PREFIX = '0000' THEN 'Dead' ELSE 'Alive' END) AS calculated_status, -- this should get calculated by Demographics.js in ehr module getting set by birth or arrival
+(CASE WHEN AN_LOCATION_PREFIX = '0000' THEN 'Dead' ELSE 'Alive' END) AS calculated_status,
 AN_SIRE_ID AS sire,
 AN_DAM_ID AS dam,
 AN_ACQ_SOURCE_INST AS origin
