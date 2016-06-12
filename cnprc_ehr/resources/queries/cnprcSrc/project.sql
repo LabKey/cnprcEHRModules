41c7efe4da76dce8600f94e4d50c787008c7cec3
Binal Patel <binalp@labkey.com>
2016-06-12 17:39:44 +0000

    Secure Issue 26632: Validate Active Assignment query

--- cnprc_ehr/resources/queries/cnprcSrc/project.sql
+++ cnprc_ehr/resources/queries/cnprcSrc/project.sql
@@ -1,18 +1,14 @@
 SELECT
 pp.PP_ID AS project,
 pr.PR_CODE AS name,
-pp.PP_AUCAAC_NUMBER AS protocol,
--- AS account,
 pr.PR_PI_NAME AS inves,
--- AS investigatorId,
 pr.PR_OI_NAME AS inves2,
-pr.PR_TITLE AS title,
-(CASE WHEN pt.PRT_TYPE = '3' THEN 'TRUE' ELSE 'FALSE' END) AS research,
--- AS use_category,
 pr.PR_BEGIN_DATE AS startdate,
 pr.PR_END_DATE AS enddate,
-pt.PRT_TYPE AS projectType,
--- AS objectid
+pr.PR_TITLE AS title,
+coalesce(pp_aucaac_number, pr_aucaac_protocol_number) AS protocol,
+(CASE WHEN pt.PRT_TYPE = '3' THEN 'TRUE' ELSE 'FALSE' END) AS research,
+pt.PRT_TYPE AS projectType
 FROM cnprcSrc.ZPROJECT pr
 LEFT JOIN cnprcSrc.ZPROJECT_PROTOCOL pp
 ON pr.PR_CODE = pp.PP_PROJECT_ID
