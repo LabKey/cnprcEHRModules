895ca129717ea9ccc75b584c980cf36df33d3cdf
Binal Patel <binalp@labkey.com>
2016-06-24 14:34:27 +0000

    Spec 26475: ETLs, Lookups, Views - Breeding, Behavior 16.2.4 M2 - added Flags and Behavior ETL, modified Protocol query to include historical protocol Ids, updated Reports.

--- cnprc_ehr/resources/queries/cnprcSrc/protocol.sql
+++ cnprc_ehr/resources/queries/cnprcSrc/protocol.sql
@@ -5,8 +5,15 @@ PRT_PI_PERSON_ID AS investigatorId,
 PRT_BEGIN_DATE AS approve,
 PRT_END_DATE AS enddate,
 PRT_TYPE AS project_type
--- AS inves,
--- AS investigatorId,
--- AS maxAnimals,
 -- AS objectid
 FROM cnprcSrc.ZPROTOCOL
+UNION -- adding historical protocols --
+SELECT DISTINCT
+PR_AUCAAC_PROTOCOL_NUMBER AS protocol,
+PR_AUCAAC_PROTOCOL_NUMBER AS title,
+NULL AS investigatorId,
+NULL AS approve,
+NULL AS enddate,
+NULL AS project_type
+-- AS objectid
+FROM cnprcSrc.ZPROJECT WHERE PR_AUCAAC_PROTOCOL_NUMBER IS NOT NULL;
