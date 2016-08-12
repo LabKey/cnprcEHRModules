60ad40c1c61b6e64618f4d58be8cfd347072a572
Binal Patel <binalp@labkey.com>
2016-08-12 15:47:02 +0000

    Spec 26837: Research Services, Primate Medicine - Code Review and Buddy Testing feedback changes, and some cleanup.

--- cnprc_ehr/resources/queries/cnprcSrc/q_clinical_observations.sql
+++ cnprc_ehr/resources/queries/cnprcSrc/q_clinical_observations.sql
@@ -1,13 +1,13 @@
 SELECT
 DI_AN_ID AS Id,
 DI_OBS_DATE AS obsDate,
-'stool' AS category,
+'Diarrhea' AS category,
 DI_OBS_CODE AS code
 FROM cnprcSrc.ZDIARRHEA
 UNION ALL
 SELECT
 PA_AN_ID AS Id,
 PA_OBS_DATE AS obsDate,
-'app' AS category,
+'Poor Appetite' AS category,
 PA_OBS_CODE AS code
 FROM cnprcSrc.ZPOOR_APP;
