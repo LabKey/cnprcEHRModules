483ab918bfcf0f45207ca90f7672c4d430681855
Ron Dashwood <rond@labkey.com>
2016-09-14 20:07:26 +0000

    Spec ID #27473 - CNPRC - Request, Billing, Admin, Training - ETLs - Update module version - Admin ETLS - Department, KeyName, Key, Person, KeyAssignment

--- cnprc_ehr/src/org/labkey/cnprc_ehr/CNPRC_EHRModule.java
+++ cnprc_ehr/src/org/labkey/cnprc_ehr/CNPRC_EHRModule.java
@@ -46,7 +46,7 @@ public class CNPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public double getVersion()
     {
-        return 16.27;
+        return 16.28;
     }
 
     @Override
