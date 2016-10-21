be495b33d54a71c82b567cef01c6a8472bc43f52
Marty Pradere <martyp@labkey.com>
2016-10-21 16:33:13 +0000

    Spec27518: Animal history automated testing updates.

--- cnprc_ehr/test/src/org/labkey/test/pages/cnprc_ehr/CNPRCAnimalHistoryPage.java
+++ cnprc_ehr/test/src/org/labkey/test/pages/cnprc_ehr/CNPRCAnimalHistoryPage.java
@@ -4,10 +4,7 @@ package org.labkey.test.pages.cnprc_ehr;
 import org.labkey.test.WebDriverWrapper;
 import org.labkey.test.WebTestHelper;
 import org.labkey.test.pages.ehr.AnimalHistoryPage;
-import org.labkey.test.util.DataRegionTable;
-import org.labkey.test.util.Ext4Helper;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CNPRCAnimalHistoryPage extends AnimalHistoryPage
 {
@@ -25,12 +22,5 @@ public class CNPRCAnimalHistoryPage extends AnimalHistoryPage
     {
         driverWrapper.beginAt(WebTestHelper.buildURL("ehr", containerPath, "animalHistory")); // TODO: Change to cnprc_ehr once they have a custom animal history page
         return new CNPRCAnimalHistoryPage(driverWrapper.getDriver());
-    }
-
-    public void refreshReport()
-    {
-        doAndWaitForPageSignal(
-                () -> waitAndClick(Ext4Helper.Locators.ext4Button("Refresh")),
-                REPORT_TAB_SIGNAL, new WebDriverWait(getDriver(), 60));
     }
 }
