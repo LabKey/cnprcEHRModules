9847ab24f3cfba87de473c815bb5f3ca9bafac32
Binal Patel <binalp@labkey.com>
2017-01-20 08:04:35 +0000

    Spec 28590 : Animal Search - Adjusted flag column name for the Search Panel; and modified current assignment query to include all assignment columns.

--- cnprc_ehr/resources/queries/study/AssignmentCurrent.sql
+++ cnprc_ehr/resources/queries/study/AssignmentCurrent.sql
@@ -1,12 +1,28 @@
 select
 y.Id,
 max(y.primaryProject) AS primaryProject,
-max(y.secondaryProjects) AS secondaryProjects
+max(y.secondaryProjects) AS secondaryProjects,
+y.date,
+y.project,
+y.projectCode,
+y.assignmentStatus,
+y.enddate,
+y.protocol,
+y.projectProtocolAssignDate,
+y.projectProtocolRelDate
 from(
 select
 x.Id,
 (CASE WHEN x.assignmentStatus='P' then max(x.projectCode) END) AS primaryProject,
-(CASE WHEN x.assignmentStatus='S' then Group_concat(x.projectCode, ', ') ELSE NULL END) AS secondaryProjects
+(CASE WHEN x.assignmentStatus='S' then Group_concat(x.projectCode, ', ') ELSE NULL END) AS secondaryProjects,
+x.date,
+x.project,
+x.projectCode,
+x.assignmentStatus,
+x.enddate,
+x.protocol,
+x.projectProtocolAssignDate,
+x.projectProtocolRelDate
 FROM assignment x where x.enddate is null
 group by x.Id, x.assignmentStatus) y
 group by y.Id
