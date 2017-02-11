9b0823450ced52aa49695dfbde8f45edb42e9f66
Binal Patel <binalp@labkey.com>
2017-02-11 01:17:13 +0000

    Removed necropsy from deaths dataset.

--- cnprc_ehr/resources/queries/cnprcSrc/q_deaths.sql
+++ cnprc_ehr/resources/queries/cnprcSrc/q_deaths.sql
@@ -17,19 +17,15 @@ SELECT
 a.AN_ID AS Id,
 a.AN_LOCATION_DATE,
 a.AN_DEATH_COMMENT AS cause,
-r.PRM_PK AS necropsy,
 a.AN_DAM_ID AS dam,
 a.AN_DEATH_TYPE AS manner,
 (CASE WHEN a.AN_ID = n.an_id THEN 'TRUE' ELSE 'FALSE' END) AS notAtCenter,
 RELOC.OBJECTID AS objectid,
 CAST (
-  GREATEST(a.date_time, IFNULL (r.date_time,to_date('01-01-1900', 'DD-MM-YYYY')),
-                        IFNULL (n.date_time,to_date('01-01-1900', 'DD-MM-YYYY')))
+  GREATEST(a.date_time, IFNULL (n.date_time,to_date('01-01-1900', 'DD-MM-YYYY')))
 AS TIMESTAMP ) AS DATE_TIME
 FROM cnprcSrc.ZANIMAL a
 JOIN cnprcSrc.ZRELOCATION RELOC ON RELOC.RELOC_AN_ID = A.AN_ID AND RELOC.RELOC_LOCATION_PREFIX = '0000'
-LEFT JOIN cnprcSrc.ZPATH_REPORT_MASTER r
-ON a.AN_ID = r.PRM_AN_ID
 LEFT JOIN (SELECT p.reloc_an_id as an_id,
   p.reloc_seq,
   p.reloc_location_prefix,
