07a08b7f1d8cc0fde97b00ddabf34c3498b6080b
Ed  Younskevicius <eyounske@labkey.com>
2017-03-16 16:02:17 +0000

    Spec 29398: CNPRC: Animal History Changes - Location   -- Add pairing indicator

--- cnprc_ehr/resources/queries/study/LocationReport.sql
+++ cnprc_ehr/resources/queries/study/LocationReport.sql
@@ -19,7 +19,32 @@ animal.demographicsActivePregnancy.conNum,
 animal.demographicsActivePregnancy.daysPregnant,
 animal.demographicsActivePregnancy.conceptionDateStatus,
 room_enclosure.supervisor,
-housing.room
+housing.room,
+(SELECT
+      CASE
+          WHEN (pair2.Id IS NULL) AND (pair1.observation <> 'AW') THEN 'DD'
+          WHEN (SELECT COUNT(*) FROM study.pairings pair3
+                WHERE pair3.Id = animal.Id
+                AND pair3.endDate IS NULL)
+                > 1
+            THEN '**'
+          -- CAST is for natural sort
+          WHEN CAST(pair1.Id.curLocation.cage AS INTEGER) < CAST(pair2.Id.curLocation.cage AS INTEGER)
+            THEN '//'
+          WHEN CAST(pair1.Id.curLocation.cage AS INTEGER) > CAST(pair2.Id.curLocation.cage AS INTEGER)
+            THEN '\\'
+          WHEN pair1.Id < pair2.Id
+            THEN '//'
+          WHEN pair1.Id > pair2.Id
+            THEN '\\'
+      END AS PairedSymbol
+  FROM study.pairings pair1
+  LEFT OUTER JOIN study.pairings pair2 ON pair2.pairId = pair1.pairId
+                                       AND pair2.Id <> animal.Id
+  WHERE pair1.Id = animal.Id
+  AND pair1.endDate IS NULL
+  AND animal.curLocation.location IS NOT NULL
+  LIMIT 1) AS pairingIndicator
 
 FROM study.animal
 INNER JOIN cnprc_ehr.cage_location_history clh ON clh.location = animal.curLocation.location
