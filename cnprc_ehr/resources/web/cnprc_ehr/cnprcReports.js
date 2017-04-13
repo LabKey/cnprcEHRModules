6d5eb6a81074b0b441af6aabf8ab17164c619ab9
Marty Pradere <martyp@labkey.com>
2017-04-13 16:50:50 +0000

    Spec 29474: Code review.

--- cnprc_ehr/resources/web/cnprc_ehr/cnprcReports.js
+++ cnprc_ehr/resources/web/cnprc_ehr/cnprcReports.js
@@ -10,11 +10,10 @@ Ext4.namespace('EHR.reports');
 //dependencies are requested, provided this module is enabled
 
 
-EHR.reports.immunizations = function(panel, tab, viewName) {
+EHR.reports.immunizations = function (panel, tab, viewName) {
     var filterArray = panel.getFilterArray(tab);
     var title = panel.getTitleSuffix();
 
-    var date = Ext4.Date.format(new Date(), LABKEY.extDefaultDateFormat);
     tab.add({
         xtype: 'ldk-querypanel',
         style: 'margin-bottom:20px;',
@@ -39,32 +38,24 @@ EHR.reports.immunizations = function(panel, tab, viewName) {
             html: '<b>Key:</b>',
             style: 'padding-bottom: 5px;'
         }, {
-            html: '<table>'
-        }, {
-            html: '<tr><td>DT:</td><td>Diphtheria + Tetanus</td></tr>'
-        }, {
-            html: '<tr><td>ET:</td><td>Equine tetanus</td></tr>'
-        }, {
-            html: '<tr><td>K :</td><td>Canine distemper</td></tr>'
-        }, {
-            html: '<tr><td>M :</td><td>Rubeola vaccine</td></tr>'
-        }, {
-            html: '<tr><td>MX:</td><td>Measles experimental</td></tr>'
-        }, {
-            html: '<tr><td>T :</td><td>Tetanus</td></tr>'
-        }, {
-            html: '<tr><td>~N:</td><td>Natural infections</td></tr>'
-        }, {
-            html: '<tr><td>~X:</td><td>Experimental</td></tr>' +
-            '</table>'
+            html: '<table>' +
+                '<tr><td>DT:</td><td>Diphtheria + Tetanus</td></tr>' +
+                '<tr><td>ET:</td><td>Equine tetanus</td></tr>' +
+                '<tr><td>K :</td><td>Canine distemper</td></tr>' +
+                '<tr><td>M :</td><td>Rubeola vaccine</td></tr>' +
+                '<tr><td>MX:</td><td>Measles experimental</td></tr>' +
+                '<tr><td>T :</td><td>Tetanus</td></tr>' +
+                '<tr><td>~N:</td><td>Natural infections</td></tr>' +
+                '<tr><td>~X:</td><td>Experimental</td></tr>' +
+                '</table>'
         }]
     });
-}
-    EHR.reports.pairingHistory = function(panel, tab, viewName){
+};
+
+EHR.reports.pairingHistory = function (panel, tab, viewName) {
     var filterArray = panel.getFilterArray(tab);
     var title = panel.getTitleSuffix();
 
-    var date = Ext4.Date.format(new Date(), LABKEY.extDefaultDateFormat);
     tab.add({
         xtype: 'ldk-querypanel',
         style: 'margin-bottom:20px;',
@@ -88,19 +79,18 @@ EHR.reports.immunizations = function(panel, tab, viewName) {
         items: [{
             html: '<b>Key:</b>',
             style: 'padding-bottom: 5px;'
-        },{
-            html: '<table>'},{
-            html: '<tr><td colspan="2"><b>Pairing Codes</b></td>                    <td colspan="2"><b>Deferment Status Code</td></tr>'},{
-            html: '<tr><td><b>CG</b></td><td>Continuous pair with grate</td>        <td><b>XS</b></td><td>Experimental deferral, single</td></tr>'},{
-            html: '<tr><td><b>CP</b></td><td>Continuous pair</td>                   <td><b>XD</b></td><td>Experimental deferral, no group mate</td></tr>'},{
-            html: '<tr><td><b>IG</b></td><td>Intermittent pair with grate</td>      <td><b>XG</b></td><td>Experimental deferral with grate</td></tr>'},{
-            html: '<tr><td><b>IP</b></td><td>Intermittent pair</td>                 <td><b>PS</b></td><td>Permanent deferral, social incompatibility</td></tr>'},{
-            html: '<tr><td><b>U</b></td><td>Unsuccessful</td>                       <td><b>AW</b></td><td>Single, awaiting pairing</td></tr>'},{
-            html: '<tr><td><b>SP</b></td><td>Socialization play, play groups</td>   <td><b>CF</b></td><td>Compatible, then fought (Retired code)</td></tr>'},{
-            html: '<tr><td><b>F</b></td><td>Fought (Retired code)</td><td></td><td></td></tr>'},{
-            html: '<tr><td><b>C</b></td><td>Compatible (Retired code)</td><td></td><td></td></tr>' +
-                    '</table>'
+        }, {
+            html: '<table>' +
+                '<tr><td colspan="2"><b>Pairing Codes</b></td>                    <td colspan="2"><b>Deferment Status Code</td></tr>' +
+                '<tr><td><b>CG</b></td><td>Continuous pair with grate</td>        <td><b>XS</b></td><td>Experimental deferral, single</td></tr>' +
+                '<tr><td><b>CP</b></td><td>Continuous pair</td>                   <td><b>XD</b></td><td>Experimental deferral, no group mate</td></tr>' +
+                '<tr><td><b>IG</b></td><td>Intermittent pair with grate</td>      <td><b>XG</b></td><td>Experimental deferral with grate</td></tr>' +
+                '<tr><td><b>IP</b></td><td>Intermittent pair</td>                 <td><b>PS</b></td><td>Permanent deferral, social incompatibility</td></tr>' +
+                '<tr><td><b>U</b></td><td>Unsuccessful</td>                       <td><b>AW</b></td><td>Single, awaiting pairing</td></tr>' +
+                '<tr><td><b>SP</b></td><td>Socialization play, play groups</td>   <td><b>CF</b></td><td>Compatible, then fought (Retired code)</td></tr>' +
+                '<tr><td><b>F</b></td><td>Fought (Retired code)</td><td></td><td></td></tr>' +
+                '<tr><td><b>C</b></td><td>Compatible (Retired code)</td><td></td><td></td></tr>' +
+                '</table>'
         }]
     });
-
-}
+};
