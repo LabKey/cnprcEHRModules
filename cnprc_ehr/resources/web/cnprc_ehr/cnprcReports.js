28d932bd04374005f92f00db60fd914af60499a8
Ron Dashwood <rond@labkey.com>
2017-04-18 22:50:48 +0000

    Spec Issue # 29401 CNPRC: Animal History - Virology History - Add query, view, and lookups. - In LinkedSchema do not throw exception on warning.

--- cnprc_ehr/resources/web/cnprc_ehr/cnprcReports.js
+++ cnprc_ehr/resources/web/cnprc_ehr/cnprcReports.js
@@ -94,3 +94,63 @@ EHR.reports.pairingHistory = function (panel, tab, viewName) {
         }]
     });
 };
+
+EHR.reports.virology = function (panel, tab, viewName){
+    var filterArray = panel.getFilterArray(tab);
+    var title = panel.getTitleSuffix();
+
+    var date = Ext4.Date.format(new Date(), LABKEY.extDefaultDateFormat);
+    tab.add({
+        xtype: 'ldk-querypanel',
+        style: 'margin-bottom:20px;',
+        queryConfig: panel.getQWPConfig({
+            schemaName: 'study',
+            queryName: 'virology',
+            viewName: viewName,
+            title: 'Virology' + title,
+            filters: filterArray.nonRemovable,
+            removeableFilters: filterArray.removable
+        })
+    });
+
+    tab.add({
+        xtype: 'panel',
+        bodyStyle: 'padding: 10px;',
+        border: false,
+        defaults: {
+            border: false
+        },
+        items: [{
+            html: '<b>Key:</b>',
+            style: 'padding-bottom: 5px;'
+        },{
+            html: '<table>' +
+             '<tr><td colspan="2"><b>Virus</b></td>                                                    <td colspan="2"><b>Result</td></tr>' +
+             '<tr><td><b>CMV</b></td><td>Cytomegalovirus</td>                                          <td><b>+</b></td><td>Positive</td></tr>' +
+             '<tr><td><b>HERB</b></td><td>Herpes B virus</td>                                          <td><b>-</b></td><td>Negative</td></tr>' +
+             '<tr><td><b>SRV</b></td><td>Simian Retrovirus, no specific serotype</td>                  <td><b>?</b></td><td>Indeterminate</td></tr>' +
+             '<tr><td><b>SRVx</b></td><td>Simian Retrovirus serotype, where x denotes serotypes</td>   <td><b></b></td><td></td></tr>' +
+             '<tr><td><b>SIV</b></td><td>Simian Immunodeficiency Virus, no specific origin</td>        <td colspan="2"><b>Sample Type</b></td><td></td></tr>' +
+             '<tr><td><b>SIVMAC</b></td><td>SIV, macaque origin</td>                                   <td><b>PBL</b></td><td>Peripheral blood lymphocytes</td></tr>' +
+             '<tr><td><b>SIVSMM</b></td><td>SIV, sooty mangabey origin</td>                            <td><b>SAL</b></td><td>Saliva</td></tr>' +
+             '<tr><td><b>SIVAGM</b></td><td>SIV, African green monkey origin</td>                      <td><b>SER</b></td><td>Serum</td></tr>' +
+             '<tr><td><b>STLV1</b></td><td>Simian T-cell leukemia virus, serotype 1</td>               <td><b>CSF</b></td><td>Cerebrospinal fluid</td></tr>' +
+             '<tr><td><b>HIV</b></td><td>Human Immunodeficiency Virus, serotype 1</td>                 <td><b>EYE</b></td><td>Tissuefluid collected from either eye</td></tr>' +
+             '<tr><td><b>HIV2</b></td><td>Human Immunodeficiency Virus, serotype 2</td>                <td><b>GEN</b></td><td>Tissuefluid collected from genitalia</td></tr>' +
+             '<tr><td><b>FOAMY</b></td><td>Simian Foamy Virus (SFV)</td>                               <td><b>OTH</b></td><td>Other</td></tr>' +
+             '<tr><td><b>RRV</b></td><td>Rhesus Rhadinovirus</td>                                      <td></td><td></td></tr>'  +
+             '<tr><td>&nbsp;</td><td></td>                                                            <td><b></b></td><td></td></tr>'  +
+             '<tr><td colspan="2"><b>Target</b></td>                                                   <td colspan="2"><b>Test Method</b></td></tr>'  +
+             '<tr><td><b>AB</b></td><td>Antibody</td>                                                  <td><b>EL</b></td><td>ELISA</td></tr>'  +
+             '<tr><td><b>AG</b></td><td>Antigen</td>                                                   <td><b>IF</b></td><td>Immunoflourescent assay</td></tr>'  +
+             '<tr><td><b>NA</b></td><td>Nucleic acid for PCR</td>                                      <td><b>MI</b></td><td>Multiplex microbead Immunoassay</td></tr>'  +
+             '<tr><td><b>VI</b></td><td>Virus Isolation</td>                                           <td><b>PC</b></td><td>PCR</td></tr>'  +
+             '<tr><td><b></b></td><td></td>                                                            <td><b>RJ</b></td><td>Raji cell assay</td></tr>'  +
+             '<tr><td><b></b></td><td></td>                                                            <td><b>VC</b></td><td>Virus culture</td></tr>'  +
+             '<tr><td><b></b></td><td></td>                                                            <td><b>WB</b></td><td>Western Blot</td></tr>'  +
+             '<tr><td><b></b></td><td></td>                                                            <td><b></b></td><td></td></tr>' +
+                    '</table>'
+        }]
+    });
+
+}
