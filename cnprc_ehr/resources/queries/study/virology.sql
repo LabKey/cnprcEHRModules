f7c554e4560d8d1ac93ebcf2d32e1ee359f3d619
Ron Dashwood <rond@labkey.com>
2017-04-21 16:15:11 +0000

    Spec Issue # 29401 CNPRC: Animal History - Virology History - Format sql - CR

--- cnprc_ehr/resources/queries/study/virology.sql
+++ cnprc_ehr/resources/queries/study/virology.sql
@@ -1,63 +1,67 @@
-select sub.*
- from
+SELECT sub.*
+ FROM
 (
-SELECT
-s.animalId as id,
-s.sampleDate as sampleDate,
-t.type as target,
-t.type as virus,
-t.type as method,
-t.results,
-t.testDoneDate,
-s.sampleType as sampleType
-FROM cnprc_pdl_linked.samples s
-join cnprc_pdl_linked.tests t on t.sample_fk = s.sample_pk
-join cnprc_pdl_linked.orders o on o.order_pk = s.order_fk
-WHERE
-t.type NOT LIKE 'ABSCN%'
-and t.results is not null
-and t.isHideOnReport = 0
-and o.orderDate is not NULL
-and o.reportDate is not null
-UNION
+  SELECT
+    s.animalId AS id,
+    s.sampleDate AS sampleDate,
+    t.type AS target,
+    t.type AS virus,
+    t.type AS method,
+    t.results,
+    t.testDoneDate,
+    s.sampleType AS sampleType
+  FROM cnprc_pdl_linked.samples s
+    JOIN cnprc_pdl_linked.tests t ON t.sample_fk = s.sample_pk
+    JOIN cnprc_pdl_linked.orders o ON o.order_pk = s.order_fk
+  WHERE
+    t.type NOT LIKE 'ABSCN%'
+    AND t.results IS NOT NULL
+    AND t.isHideOnReport = 0
+    AND o.orderDate IS NOT NULL
+    AND o.reportDate IS NOT NULL
 
-SELECT
-s.animalId as id,
-s.sampleDate as sampleDate,
-t.type || '-' || st.type  as target,
-t.type || '-' || st.type  as virus,
-t.type || '-' || st.type  as method,
-st.results,
-st.testDoneDate,
-s.sampleType as sampleType
-FROM cnprc_pdl_linked.samples s
-join cnprc_pdl_linked.tests t on t.sample_fk = s.sample_pk
-join cnprc_pdl_linked.sub_tests st on st.test_fk = t.test_pk
-join cnprc_pdl_linked.orders o on o.order_pk = s.order_fk
-WHERE
-st.type NOT LIKE 'ABSCN%'
-and st.results is not null
-and st.isHideOnReport = 0
-and o.orderDate is not NULL
-and o.reportDate is not null
-UNION
-SELECT
-v.id,
-v.date as sampleDate,
-v.target as target,
-v.virus as virus,
-v.method as method,
-v.result as results,
-v.testDate as testDoneDate,
-v.sampleType as sampleType
-FROM study.virology v
-WHERE V.LAB NOT IN ('SRRL', 'CRPRC')
+  UNION
+  
+  SELECT
+    s.animalId AS id,
+    s.sampleDate AS sampleDate,
+    t.type || '-' || st.type  AS target,
+    t.type || '-' || st.type  AS virus,
+    t.type || '-' || st.type  AS method,
+    st.results,
+    st.testDoneDate,
+    s.sampleType AS sampleType
+  FROM cnprc_pdl_linked.samples s
+    JOIN cnprc_pdl_linked.tests t ON t.sample_fk = s.sample_pk
+    JOIN cnprc_pdl_linked.sub_tests st ON st.test_fk = t.test_pk
+    JOIN cnprc_pdl_linked.orders o ON o.order_pk = s.order_fk
+  WHERE
+    st.type NOT LIKE 'ABSCN%'
+    AND st.results IS NOT NULL
+    AND st.isHideOnReport = 0
+    AND o.orderDate IS NOT NULL
+    AND o.reportDate IS NOT NULL
+
+  UNION
+
+  SELECT
+    v.id,
+    v.date AS sampleDate,
+    v.target AS target,
+    v.virus AS virus,
+    v.method AS method,
+    v.result AS results,
+    v.testDate AS testDoneDate,
+    v.sampleType AS sampleType
+  FROM study.virology v
+  WHERE
+    V.LAB NOT IN ('SRRL', 'CRPRC')
 )sub
-join study.animals a on a.id = sub.id
-where
-not (
-sub.testDoneDate is  null AND
-sub.virus is null AND
-sub.method is null
-)
-and results is not null
+  JOIN study.animals a ON a.id = sub.id
+  WHERE
+    NOT (
+      sub.testDoneDate IS NULL AND
+      sub.virus IS NULL AND
+      sub.method IS NULL
+    )
+    AND results IS NOT NULL
