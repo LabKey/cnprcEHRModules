7f9f62429a3016ca8e1bec0ca02042f18fb0e5d7
Ron Dashwood <rond@labkey.com>
2017-05-11 22:39:35 +0000

    Spec Issue # 29479 - CNPRC: Animal History - Diarrhea Calendar - Add standard calendar view via reports.tsv

---
+++ cnprc_ehr/resources/queries/study/diarrheaCalendar.sql
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011-2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+SELECT
+i.Id,
+i.year,
+i.monthName,
+i.monthNum,
+i.day,
+max(i.category) as category
+
+FROM (
+
+SELECT
+  i.Id,
+  i.date,
+  convert(year(i.date), integer) as year,
+  monthname(i.date) AS monthName,
+  convert(month(i.date), integer) AS monthNum,
+  --convert(week(i.date), integer) as Week,
+  convert(dayofmonth(i.date), integer) as day,
+  'D' as category
+
+FROM study.clinical_observations i
+WHERE i.category = 'diarrhea'
+) i
+GROUP BY i.id, i.year, i.monthName, i.monthNum, i.day
+PIVOT category BY day
