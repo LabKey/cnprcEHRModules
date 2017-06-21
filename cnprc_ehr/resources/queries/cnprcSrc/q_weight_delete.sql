e66dd53fd3d3e193ee1f6b570fb4f128ce564034
Binal Patel <binalp@labkey.com>
2017-06-21 08:14:28 +0000

    Modified to remove duplicate weights. Added birth weights back.

--- cnprc_ehr/resources/queries/cnprcSrc/q_weight_delete.sql
+++ cnprc_ehr/resources/queries/cnprcSrc/q_weight_delete.sql
@@ -18,15 +18,92 @@ OBJECTID as objectid,
 DATE_TIME
 FROM cnprcSrc.AWEIGHING
 WHERE WT_AUD_CODE = 'D'
+
 UNION
+
 SELECT
 OBJECTID as objectid,
 DATE_TIME
 FROM cnprcSrc_aud.APRIMED_TREATMENT
 WHERE AUD_CODE = 'D'
+
 UNION
+
 SELECT
 OBJECTID as objectid,
 DATE_TIME
 FROM cnprcSrc_aud.ABIO_BEHAVIORAL_ASSESSMENT
 WHERE BBA_AUD_CODE = 'D'
+
+UNION
+
+-- delete duplicates
+SELECT
+dupes.objectid,
+dupes.date_time
+FROM(
+SELECT
+wt.Id,
+wt.weightDate,
+wt.Weight,
+max(wt.objectid) AS objectid,
+max(wt.date_time) AS date_time
+FROM
+(SELECT
+WT_AN_ID AS Id,
+WT_DATE AS weightDate,
+WT_WEIGHT_KG AS Weight,
+OBJECTID AS objectid,
+DATE_TIME AS date_time
+FROM cnprcSrc.ZWEIGHING
+WHERE WT_WEIGHT_KG IS NOT NULL
+
+UNION ALL
+
+SELECT
+AN_ID AS Id,
+AN_BIRTH_DATE AS weightDate,
+AN_BIRTH_WT_KG AS Weight,
+OBJECTID AS objectid,
+DATE_TIME AS date_time
+FROM cnprcSrc.ZANIMAL ani
+WHERE
+AN_BIRTH_WT_KG IS NOT NULL AND
+NOT EXISTS (SELECT NULL FROM cnprcSrc.ZWEIGHING weight WHERE weight.WT_AN_ID = ani.AN_ID AND weight.WT_DATE = ani.AN_BIRTH_DATE)
+
+UNION ALL
+
+SELECT DISTINCT
+PT_AN_ID AS Id,
+PT_START_DATE AS weightDate,
+PT_AN_WT_USED AS Weight,
+OBJECTID AS objectid,
+DATE_TIME AS date_time
+FROM cnprcSrc.ZPRIMED_TREATMENT trt
+WHERE
+PT_AN_WT_USED IS NOT NULL
+AND
+PT_AN_WT_USED <> PT_AN_WT_DB
+AND
+PT_RX_INVALID_OR_NEVER_GIVEN IS NULL AND
+trt.PT_TREATMENT_TYPE||trt.PT_SEQ = (SELECT MIN((t2.PT_TREATMENT_TYPE||t2.PT_SEQ)) FROM cnprcSrc.ZPRIMED_TREATMENT t2
+                                     WHERE t2.PT_AN_ID = trt.PT_AN_ID AND t2.PT_START_DATE = trt.PT_START_DATE) AND
+NOT EXISTS (SELECT NULL FROM cnprcSrc.ZWEIGHING weight WHERE weight.WT_AN_ID = trt.PT_AN_ID AND
+            weight.WT_DATE = trt.PT_START_DATE AND weight.WT_WEIGHT_KG IS NOT NULL)
+
+UNION ALL
+
+SELECT
+ANIMID AS Id,
+TEST_DAT AS weightDate,
+WEIGHT AS Weight,
+OBJECTID AS objectid,
+DATE_TIME AS date_time
+FROM cnprcSrc.ZBIO_BEHAVIORAL_ASSESSMENT
+WHERE
+WEIGHT IS NOT NULL) wt
+GROUP BY
+wt.Id,
+wt.weightDate,
+wt.Weight
+HAVING count(*) > 1) dupes
