693cce0e1bf6fdadec1416c3c3288f895f4ef942
Ed  Younskevicius <eyounske@labkey.com>
2017-11-17 00:30:11 +0000

    merge from modules 17.3 through 55061

--- cnprc_ehr/src/org/labkey/cnprc_ehr/table/CNPRC_EHRCustomizer.java
+++ cnprc_ehr/src/org/labkey/cnprc_ehr/table/CNPRC_EHRCustomizer.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.labkey.cnprc_ehr.table;
 
 import org.labkey.api.data.AbstractTableInfo;
