b418897db5162c52cfbaf12b936db61ea7e9e642
Binal Patel <binalp@labkey.com>
2017-12-10 23:59:50 +0000

    Support Ticket 32210: Assignments and Groups - Project Assignment History; error with released date -- only close Assigments after Departure if animal's final status is not at the center.

---
+++ cnprc_ehr/resources/queries/study/Departure.js
@@ -0,0 +1,18 @@
+require("ehr/triggers").initScript(this);
+
+function onInit(event, helper){
+    helper.setScriptOptions({
+        requiresStatusRecalc: true
+    });
+
+    helper.decodeExtraContextProperty('departuresInTransaction');
+
+}
+
+EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.ON_BECOME_PUBLIC, 'study', 'Departure', function(scriptErrors, helper, row, oldRow) {
+    helper.registerDeparture(row.Id, row.date);
+
+    //this will close any existing assignments, housing and treatment records
+    if(row.nextreloctype != undefined && row.nextreloctype != 'Here' )
+        helper.onDeathDeparture(row.Id, row.date);
+});
