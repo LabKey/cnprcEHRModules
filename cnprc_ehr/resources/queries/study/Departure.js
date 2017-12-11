a3dde8fb45b67db25d44b733ddeb85329d6c4ebe
Binal Patel <binalp@labkey.com>
2017-12-11 21:12:30 +0000

    Support Ticket 32210: Assignments and Groups - Project Assignment History; error with released date -- unregister EHR registered handler for Departure.

--- cnprc_ehr/resources/queries/study/Departure.js
+++ cnprc_ehr/resources/queries/study/Departure.js
@@ -12,7 +12,11 @@ function onInit(event, helper){
 EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.ON_BECOME_PUBLIC, 'study', 'Departure', function(scriptErrors, helper, row, oldRow) {
     helper.registerDeparture(row.Id, row.date);
 
-    //this will close any existing assignments, housing and treatment records
-    if(row.nextreloctype != undefined && row.nextreloctype != 'Here' )
-        helper.onDeathDeparture(row.Id, row.date);
+    if(helper.isETL())
+    {
+        //this will close any existing assignments, housing and treatment records
+        if(row.nextRelocType != null && row.nextRelocType != 'Here' ) {
+            helper.onDeathDeparture(row.Id, row.date);
+        }
+    }
 });
--- cnprc_ehr/resources/scripts/cnprc_triggers.js
+++ cnprc_ehr/resources/scripts/cnprc_triggers.js
@@ -10,6 +10,9 @@ exports.init = function (EHR) {
 
     EHR.Server.TriggerManager.registerHandler(EHR.Server.TriggerManager.Events.INIT, function(event, helper, EHR){
 
+        //unregister so that only CNPRC's Departure.js gets used (otherwise EHR's Departure.js seem to override CNPRC's Departure.js)
+        EHR.Server.TriggerManager.unregisterAllHandlersForQueryNameAndEvent('study', 'Departure', EHR.Server.TriggerManager.Events.ON_BECOME_PUBLIC);
+
         if(helper.isETL()) {
             helper.setScriptOptions({
                 cacheAccount: false,
