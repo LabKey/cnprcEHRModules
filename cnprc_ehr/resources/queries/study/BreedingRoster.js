a7df74aae7c70b3e90d1e04049693ca14c2b3561
Ed  Younskevicius <eyounske@labkey.com>
2018-03-13 17:09:55 +0000

    Spec Issue 33308: Breeding Management (Breeding Registration)   -- Add better validation for non-alive animals

--- cnprc_ehr/resources/queries/study/BreedingRoster.js
+++ cnprc_ehr/resources/queries/study/BreedingRoster.js
@@ -25,14 +25,16 @@ function onUpsert(helper, scriptErrors, row, oldRow) {
                     }
                     maleEnemiesSoFar[maleEnemy] = true;
 
-                    if (row[indexString] && mainAnimalIsMaleFlag) {
+                    if (row[indexString]) {
                         EHR.Server.Utils.findDemographics({
                             participant: row[indexString],
                             helper: helper,
                             scope: this,
                             callback: function (data2) {
                                 if (data2) {
-                                    if (data2['gender/origGender'] && data2['gender/origGender'] === 'M')
+                                    if (data2.calculated_status !== 'Alive')
+                                        EHR.Server.Utils.addError(scriptErrors, indexString, 'Status of ' + indexString + ' ' + data2['Id'] + ' is: ' + data2.calculated_status, 'INFO');
+                                    if (mainAnimalIsMaleFlag && data2['gender/origGender'] && data2['gender/origGender'] === 'M')
                                         EHR.Server.Utils.addError(scriptErrors, indexString, 'Animal ' + row['Id'] + ' and ' + indexString + ' ' + data2['Id'] + ' are both male, male-male enemies not allowed', 'ERROR');
                                 }
                             }
