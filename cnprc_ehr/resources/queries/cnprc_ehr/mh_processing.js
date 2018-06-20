1559d0af2c4d6797930638c1ca346fb66fa88f9d
Binal Patel <binalp@labkey.com>
2018-06-20 19:41:35 +0000

    33609: MH: Validate and User Edit - Bug Fixes For hand entered mh processing data, 1. Hide cols with auto generated vals in the Insert view. 2. Add observation type, and set transferredToMHObs flag to false (decision to transfer data to study.morningHealthObs depends on these values being set).

--- cnprc_ehr/resources/queries/cnprc_ehr/mh_processing.js
+++ cnprc_ehr/resources/queries/cnprc_ehr/mh_processing.js
@@ -2,4 +2,15 @@ require("ehr/triggers").initScript(this);
 
 function onInsert(helper, scriptErrors, row) {
     row.objectid = row.objectid || LABKEY.Utils.generateUUID().toUpperCase();
+
+    var vals = row.data.split(',');
+
+    if (vals[4]) {
+        row.observationType = 'A'; //Animal observation
+    }
+    else if (vals[16] && vals[17]) {
+        row.observationType = 'R'; //Room observation
+    }
+
+    row.transferredToMhObs = false; //default is always false.
 }
