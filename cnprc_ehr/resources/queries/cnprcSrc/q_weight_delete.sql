eb8313a6705754c0b235687337b2b65edd2c226e
Binal Patel <binalp@labkey.com>
2018-07-19 16:50:36 +0000

    Spec 34252 : Pathology Report : Reformatted, and refactored weights delete to include conception.

--- cnprc_ehr/resources/queries/cnprcSrc/q_weight_delete.sql
+++ cnprc_ehr/resources/queries/cnprcSrc/q_weight_delete.sql
@@ -14,24 +14,24 @@
  * limitations under the License.
  */
 SELECT
-OBJECTID as objectid,
-DATE_TIME
+  OBJECTID AS objectid,
+  DATE_TIME
 FROM cnprcSrc.AWEIGHING
 WHERE WT_AUD_CODE = 'D'
 
 UNION
 
 SELECT
-OBJECTID as objectid,
-DATE_TIME
+  OBJECTID AS objectid,
+  DATE_TIME
 FROM cnprcSrc_aud.APRIMED_TREATMENT
 WHERE AUD_CODE = 'D'
 
 UNION
 
 SELECT
-OBJECTID as objectid,
-DATE_TIME
+  OBJECTID AS objectid,
+  DATE_TIME
 FROM cnprcSrc_aud.ABIO_BEHAVIORAL_ASSESSMENT
 WHERE BBA_AUD_CODE = 'D'
 
@@ -39,79 +39,91 @@ UNION
 
 -- delete duplicates
 SELECT
-dupes.objectid,
-dupes.date_time
-FROM(
-SELECT
-weights.Id,
-weights.weightDate,
-weights.Weight,
-max((CASE WHEN weights.weightTattooFlag IS NULL AND weights.bodyConditionScore IS NULL THEN weights.objectid END)) AS objectId,
-max((CASE WHEN weights.weightTattooFlag IS NULL AND weights.bodyConditionScore IS NULL THEN weights.date_time END)) AS date_time
-FROM
-(SELECT
-WT_AN_ID AS Id,
-WT_DATE AS weightDate,
-WT_WEIGHT_KG AS Weight,
-WT_BODY_CONDITION_SCORE AS bodyConditionScore,
-WT_TATTOO_FLAG AS weightTattooFlag,
-OBJECTID AS objectid,
-DATE_TIME AS date_time
-FROM cnprcSrc.ZWEIGHING
-WHERE WT_WEIGHT_KG IS NOT NULL
+  dupes.objectid,
+  dupes.date_time
+FROM (
+       SELECT
+         weights.Id,
+         weights.weightDate,
+         weights.Weight,
+         max((CASE WHEN weights.weightTattooFlag IS NULL AND weights.bodyConditionScore IS NULL
+           THEN weights.objectid END))  AS objectId,
+         max((CASE WHEN weights.weightTattooFlag IS NULL AND weights.bodyConditionScore IS NULL
+           THEN weights.date_time END)) AS date_time
+       FROM
+         (SELECT
+            WT_AN_ID                AS Id,
+            WT_DATE                 AS weightDate,
+            WT_WEIGHT_KG            AS Weight,
+            WT_BODY_CONDITION_SCORE AS bodyConditionScore,
+            WT_TATTOO_FLAG          AS weightTattooFlag,
+            OBJECTID                AS objectid,
+            DATE_TIME               AS date_time
+          FROM cnprcSrc.ZWEIGHING
+          WHERE WT_WEIGHT_KG IS NOT NULL
 
-UNION ALL
+          UNION ALL
 
-SELECT
-AN_ID AS Id,
-AN_BIRTH_DATE AS weightDate,
-AN_BIRTH_WT_KG AS Weight,
-NULL AS bodyConditionScore,
-NULL AS weightTattooFlag,
-OBJECTID AS objectid,
-DATE_TIME AS date_time
-FROM cnprcSrc.ZANIMAL ani
-WHERE
-AN_BIRTH_WT_KG IS NOT NULL AND
-NOT EXISTS (SELECT NULL FROM cnprcSrc.ZWEIGHING weight WHERE weight.WT_AN_ID = ani.AN_ID AND weight.WT_DATE = ani.AN_BIRTH_DATE)
+          SELECT
+            (CASE WHEN CON_OFFSPRING_ID IS NULL
+              THEN CON_NO
+             ELSE CON_OFFSPRING_ID END) AS Id, -- for still birth use conception number, else use offspring id
+            CON_TERM_DATE               AS weightDate,
+            CON_BIRTH_WT_KG             AS Weight,
+            NULL                        AS bodyConditionScore,
+            NULL                        AS weightTattooFlag,
+            OBJECTID                    AS objectid,
+            DATE_TIME                   AS date_time
+          FROM cnprcSrc.ZCONCEPTION concep
+          WHERE
+            CON_TERM_DATE IS NOT NULL AND
+            CON_BIRTH_WT_KG IS NOT NULL AND
+            NOT EXISTS(SELECT NULL
+                       FROM cnprcSrc.ZWEIGHING weight
+                       WHERE weight.WT_AN_ID = COALESCE(concep.CON_OFFSPRING_ID, concep.CON_NO) AND
+                             weight.WT_DATE = concep.CON_TERM_DATE)
 
-UNION ALL
+          UNION ALL
 
-SELECT DISTINCT
-PT_AN_ID AS Id,
-PT_START_DATE AS weightDate,
-PT_AN_WT_USED AS Weight,
-NULL AS bodyConditionScore,
-NULL AS weightTattooFlag,
-OBJECTID AS objectid,
-DATE_TIME AS date_time
-FROM cnprcSrc.ZPRIMED_TREATMENT trt
-WHERE
-PT_AN_WT_USED IS NOT NULL
-AND
-PT_AN_WT_USED <> PT_AN_WT_DB
-AND
-PT_RX_INVALID_OR_NEVER_GIVEN IS NULL AND
-trt.PT_TREATMENT_TYPE||trt.PT_SEQ = (SELECT MIN((t2.PT_TREATMENT_TYPE||t2.PT_SEQ)) FROM cnprcSrc.ZPRIMED_TREATMENT t2
-                                     WHERE t2.PT_AN_ID = trt.PT_AN_ID AND t2.PT_START_DATE = trt.PT_START_DATE) AND
-NOT EXISTS (SELECT NULL FROM cnprcSrc.ZWEIGHING weight WHERE weight.WT_AN_ID = trt.PT_AN_ID AND
-            weight.WT_DATE = trt.PT_START_DATE AND weight.WT_WEIGHT_KG IS NOT NULL)
+          SELECT DISTINCT
+            PT_AN_ID      AS Id,
+            PT_START_DATE AS weightDate,
+            PT_AN_WT_USED AS Weight,
+            NULL          AS bodyConditionScore,
+            NULL          AS weightTattooFlag,
+            OBJECTID      AS objectid,
+            DATE_TIME     AS date_time
+          FROM cnprcSrc.ZPRIMED_TREATMENT trt
+          WHERE
+            PT_AN_WT_USED IS NOT NULL
+            AND
+            PT_AN_WT_USED <> PT_AN_WT_DB
+            AND
+            PT_RX_INVALID_OR_NEVER_GIVEN IS NULL AND
+            trt.PT_TREATMENT_TYPE || trt.PT_SEQ = (SELECT MIN((t2.PT_TREATMENT_TYPE || t2.PT_SEQ))
+                                                   FROM cnprcSrc.ZPRIMED_TREATMENT t2
+                                                   WHERE t2.PT_AN_ID = trt.PT_AN_ID AND
+                                                         t2.PT_START_DATE = trt.PT_START_DATE) AND
+            NOT EXISTS(SELECT NULL
+                       FROM cnprcSrc.ZWEIGHING weight
+                       WHERE weight.WT_AN_ID = trt.PT_AN_ID AND
+                             weight.WT_DATE = trt.PT_START_DATE AND weight.WT_WEIGHT_KG IS NOT NULL)
 
-UNION ALL
+          UNION ALL
 
-SELECT
-ANIMID AS Id,
-TEST_DAT AS weightDate,
-WEIGHT AS Weight,
-NULL AS bodyConditionScore,
-NULL AS weightTattooFlag,
-OBJECTID AS objectid,
-DATE_TIME AS date_time
-FROM cnprcSrc.ZBIO_BEHAVIORAL_ASSESSMENT
-WHERE
-WEIGHT IS NOT NULL) weights
-GROUP BY
-weights.Id,
-weights.weightDate,
-weights.Weight
-HAVING count(*) > 1) dupes
+          SELECT
+            ANIMID    AS Id,
+            TEST_DAT  AS weightDate,
+            WEIGHT    AS Weight,
+            NULL      AS bodyConditionScore,
+            NULL      AS weightTattooFlag,
+            OBJECTID  AS objectid,
+            DATE_TIME AS date_time
+          FROM cnprcSrc.ZBIO_BEHAVIORAL_ASSESSMENT
+          WHERE
+            WEIGHT IS NOT NULL) weights
+       GROUP BY
+         weights.Id,
+         weights.weightDate,
+         weights.Weight
+       HAVING count(*) > 1) dupes
