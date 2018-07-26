a66409c11cb343499f9a6e941184b21ff94042f5
Ankur Juneja <ankurj@labkey.com>
2018-07-26 18:05:29 +0000

    Issue 34864 : fix gestation days in hybrid report

--- cnprc_ehr/resources/queries/study/Gestation.sql
+++ cnprc_ehr/resources/queries/study/Gestation.sql
@@ -15,8 +15,8 @@
  */
 SELECT pregnancyConfirmations.Id,
 concat(
-  cast(age(pregnancyConfirmations.conception,curdate()) AS VARCHAR),
-  pregnancyConfirmations.conceptionDateStatus
+    cast(pregnancyConfirmations.gestationDays as varchar),
+    pregnancyConfirmations.conceptionDateStatus
 ) Gestation
 FROM pregnancyConfirmations
-where pregnancyConfirmations.termDate is null
+where pregnancyConfirmations.offSpringId is null
