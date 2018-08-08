846e3aa426e1908d6774c109639db232e6b0fd71
Binal Patel <binalp@labkey.com>
2018-08-08 15:05:23 +0000

    CNPRC Support Ticket 35043: Error on Pairings ETL : Moved data entry related validation within the isETL check (otherwise this was causing Pairing ETL to take a very long time due to an ajax call for each animal)

--- cnprc_ehr/resources/queries/study/pairings.js
+++ cnprc_ehr/resources/queries/study/pairings.js
@@ -7,30 +7,31 @@ function onInsert(helper, scriptErrors, row){
 
 function onUpsert(helper, scriptErrors, row, oldRow) {
     if (!helper.isETL()) {
-    }
-    if(row.id && row.pairedWithId) {
-        EHR.Server.Validation.validateAnimal(helper, scriptErrors, row, 'Id');
-        EHR.Server.Validation.validateAnimal(helper, scriptErrors, row, 'pairedWithId');
-        LABKEY.Query.selectRows({
-            requiredVersion: 9.1,
-            schemaName: 'study',
-            queryName: 'housing',
-            columns: ['room'],
-            scope: this,
-            filterArray: [
-                LABKEY.Filter.create('Id',row.id+';'+row.pairedWithId, LABKEY.Filter.Types.IN)],
-            success: function (results) {
-                if (results && results.rows && results.rows.length >= 1) {
-                    var animal1_room = results['rows'][0]['room']['value'];
-                    var animal2_room = results['rows'][1]['room']['value'];
-                    if(animal1_room !== animal2_room){
-                        EHR.Server.Utils.addError(scriptErrors, 'Id', ' Animals are not in the same room', 'ERROR');
+
+        if (row.id && row.pairedWithId) {
+            EHR.Server.Validation.validateAnimal(helper, scriptErrors, row, 'Id');
+            EHR.Server.Validation.validateAnimal(helper, scriptErrors, row, 'pairedWithId');
+            LABKEY.Query.selectRows({
+                requiredVersion: 9.1,
+                schemaName: 'study',
+                queryName: 'housing',
+                columns: ['room'],
+                scope: this,
+                filterArray: [
+                    LABKEY.Filter.create('Id', row.id + ';' + row.pairedWithId, LABKEY.Filter.Types.IN)],
+                success: function (results) {
+                    if (results && results.rows && results.rows.length >= 1) {
+                        var animal1_room = results['rows'][0]['room']['value'];
+                        var animal2_room = results['rows'][1]['room']['value'];
+                        if (animal1_room !== animal2_room) {
+                            EHR.Server.Utils.addError(scriptErrors, 'Id', ' Animals are not in the same room', 'ERROR');
+                        }
                     }
+                },
+                failure: function (error) {
+                    console.log(error);
                 }
-            },
-            failure: function (error) {
-                console.log(error);
-            }
-        });
+            });
+        }
     }
 }
